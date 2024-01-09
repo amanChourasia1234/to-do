1e02ea0e7cde9760f690af97c793b4cf1f883ff3
Aman Chourasia <amanchourasia2002@gmail.com>
2024-01-09 22:40:09 +0530

    package.json updated

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import todosRoutes from './routes/todos.js';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import todosRoutes from "./routes/todos.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 dotenv.config();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/todos', todosRoutes);
+app.use("/todos", todosRoutes);
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const mongodb =
-  'mongodb+srv://aman:aman@cluster0.wgi0196.mongodb.net/?retryWrites=true&w=majority';
+  "mongodb+srv://aman:aman@cluster0.wgi0196.mongodb.net/?retryWrites=true&w=majority";
 
-app.get('/', (req, res) => {
-  res.send('Welcome to our Webpage');
+app.get("/", (req, res) => {
+  res.send("Welcome to our Webpage");
 });
 
 const PORT = process.env.PORT || 5000;
@@ -32,4 +36,4 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
